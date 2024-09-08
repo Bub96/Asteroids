9693c17a4289640af35e05c308d4cf60f3f3023f
Bub96 <loganeck197@gmail.com>
2024-09-07 21:21:46 -0500

    added turn speed and player speed to move around

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
-    pygame.init
+    pygame.init()
+    Clock = pygame.time.Clock()
+    dt = 0
     print('Starting asteroids!')
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
+    player1 =Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = Clock.tick(60) / 1000
         screen.fill((0, 0, 0))
+        player1.update(dt)
+        player1.draw(screen)
         pygame.display.flip()
 if __name__ == "__main__":
     main()
